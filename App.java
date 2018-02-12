6c341e2db10ba30d0b2843cbe3cac3027d20f46d
deepika-srivastav <36186630+deepika-srivastav@users.noreply.github.com>
2018-02-12 15:32:50 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package org.DigitasLBi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.prinln("Hi ganesh.....");
+    }
+}
